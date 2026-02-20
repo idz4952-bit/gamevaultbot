e136547057c1050b9f8834db3fc65829fb110536
idz4952-bit <idz4952@gmail.com>
2026-02-20 16:51:28 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,24 +1,23 @@
 import os
-import logging
+import asyncio
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-
-logging.basicConfig(level=logging.INFO)
 
 TOKEN = os.getenv("TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("🎮 مرحبا بك في GameVault 🔐")
+    await update.message.reply_text("🎮 مرحبا بك في GameVault 🔒")
 
-def main():
+async def main():
     if not TOKEN:
-        raise RuntimeError("TOKEN is missing")
+        raise RuntimeError("BOT_TOKEN is missing")
 
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
 
-    logging.info("Bot is starting polling...")
-    app.run_polling()
+    print("Bot is running...")
+    await app.run_polling()
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
+
